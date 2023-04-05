37257f50862bcb879f21cebecaa39ff9e8211fd2
nezkh <f.marchant.aravena@gmail.com>
2023-04-05 10:55:28 -0400

    greetingF.jsx agregamos birthday

--- src/components/pure/greetingF.jsx
+++ src/components/pure/greetingF.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 
 const GreetingF = (props) => {
+
+    // Breve introducción a useState
+    // const [variable, método para actualizarlo] = useState(valor inicial)
+    const [age, setage] = useState(29)
+
+    const birthday = () => {
+        // actualizamos el Sate
+        setage(age + 1)
+    }
+
     return (
         <div>
                 <h1>
                     ¡Hola! { props.name } desde componente funcional
                 </h1>
-                {/* <h2>
-                    Tu edad es de: { this.state.age }
-                </h2> */}
-                {/* <div>
-                    <button onClick={this.birthday}>
+                <h2>
+                    Tu edad es de: { age }
+                </h2>
+                <div>
+                    <button onClick={birthday}>
                         Cumplir años
                     </button>
-                </div> */}
+                </div>
             </div>
     );
 };
