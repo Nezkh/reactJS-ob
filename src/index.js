3fc9495b41bdfe9fa917e7d18290d5b7ba3ecaf7
nezkh <f.marchant.aravena@gmail.com>
2023-07-12 23:57:34 -0400

    ex6 carga react router

--- src/index.js
+++ src/index.js
@@ -8,13 +8,23 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
+import AppRouting from './AppRouting';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <AppRouting />
+  </BrowserRouter>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
